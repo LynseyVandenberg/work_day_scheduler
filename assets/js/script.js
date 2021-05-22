3b8756073d8f1b056ae09a827271e06ed6b1aec4
Lynsey Vandenberg <lynsey.vandenberg@gmail.com>
2021-05-22 17:03:14 -0400

    updates to javascript file and edits to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,56 @@
-// display current day at the top of the calender (and time?)
+// display current day at the top of the calender
+var todaysDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todaysDate);
+
+// $(function() { possible shorthand ?
+$(document).ready(function () {
+    // when user clicks save button, jquery/pull the description and id info and save it in local storage
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, text);
+    })
+
+    function timeMachine() {
+        var currentTime = moment().hour();
+
+        // loop over time blocks
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            // this compares the currentTime to the blockTime and assings a color for present, past and future
+            if (blockTime < currentTime) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === currentTime) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+            }
+        })
+    }
+
+    //get saved #hour text from local storage
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+    
+    timeMachine();
+
+})
